4cc062328f2388a3abef35d1a3061e58a05f6695
Helga151 <ogerlich151@gmail.com>
2022-12-01 10:53:45 +0100

    Split code to individual programs

---
+++ app/app/models.py
@@ -0,0 +1,28 @@
+from config import *
+# from forms import *
+
+class User(db.Model):
+    __tablename__ = "users"
+    __table_args__ = {'quote': False, 'schema': "filmweb"}
+
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(64), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    register_date = db.Column(db.Date)
+    role = db.Column(db.String(5))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __init__(self, login, password_hash, role):
+        self.login = login
+        self.password_hash = password_hash
+        self.role = role
